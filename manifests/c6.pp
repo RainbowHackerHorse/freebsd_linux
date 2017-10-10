0497bd6cdf33c64fed9f53b7c59f6739794bfb92
Rainbow <rainbow@hacker.horse>
2017-10-10 12:45:29 -0700

    add ZFS datasets

--- manifests/c6.pp
+++ manifests/c6.pp
@@ -3,6 +3,19 @@
 #
 class freebsd_linux::c6 {
   package { 'linux-c6':
-    ensure => latest,
+    ensure  => latest,
+    require => Zfs::Create['zroot/compat/linux'],
+  }
+  service { 'linux':
+    enable => true,
+  }
+  zfs::create { 'zroot/compat':
+    ensure     => present,
+    filesystem => '/compat',
+  }
+  zfs::create { 'zroot/compat/linux':
+    ensure     => present,
+    filesystem => '/compat/linux',
+    require    => Zfs::Create['zroot/compat'],
   }
 }
--- manifests/c7.pp
+++ manifests/c7.pp
@@ -3,6 +3,19 @@
 #
 class freebsd_linux::c7 {
   package { 'linux-c7':
-    ensure => latest,
+    ensure  => latest,
+    require => Zfs::Create['zroot/compat/linux'],
+  }
+  service { 'linux':
+    enable => true,
+  }
+  zfs::create { 'zroot/compat':
+    ensure     => present,
+    filesystem => '/compat',
+  }
+  zfs::create { 'zroot/compat/linux':
+    ensure     => present,
+    filesystem => '/compat/linux',
+    require    => Zfs::Create['zroot/compat'],
   }
 }
